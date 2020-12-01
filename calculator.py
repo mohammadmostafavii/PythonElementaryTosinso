15f1ad314e0882a92fdcea084d5321462c924dbf
MohammadMostafavi <mr.mohammadmostafavi@gmail.com>
2020-12-01 20:58:53 +0330

    In this lecture we learn how to work with 'input' and 'output' and 'type change' in python. and we write the version 1 of calculator project.

---
+++ calculator.py
@@ -0,0 +1,12 @@
+# Version 1
+# This Version include "input", "output" and "Type Change"
+
+num1 = input("Please Enter 1st Number: ")
+num2 = input("Please Enter 2st Number: ")
+
+# وقتی کاربر ما عدد را وارد می کند این عدد به صورت string ذخیره میشود و باید آن را به integer تبدیل کنیم،
+# برای انجام این کار از تابع int استفاده می کنیم.
+
+result = int(num1) + int(num2)
+
+print(num1, "+", num2, "=", result)
