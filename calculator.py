5d32049fbf259decf54cbd1e7e8b9575a9565b98
MohammadMostafavi <mr.mohammadmostafavi@gmail.com>
2020-12-01 21:49:35 +0330

    add format function to project

--- calculator.py
+++ calculator.py
@@ -7,6 +7,11 @@ num2 = input("Please Enter 2st Number: ")
 # وقتی کاربر ما عدد را وارد می کند این عدد به صورت string ذخیره میشود و باید آن را به integer تبدیل کنیم،
 # برای انجام این کار از تابع int استفاده می کنیم.
 
-result = int(num1) + int(num2)
+sum = int(num1) + int(num2)
+division = int(num1) / int(num2)
 
-print(num1, "+", num2, "=", result)
+print(num1, "+", num2, "=", sum)
+
+# .2f --> یعنی تا دو رقم اعشار را نشان بده
+
+print("{0} / {1} = {2:.2f}".format(num1, num2, division))
