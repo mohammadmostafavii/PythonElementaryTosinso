696cdbe46d66bb520792e5309d67cf3e0abafab4
MohammadMostafavi <mr.mohammadmostafavi@gmail.com>
2020-12-03 20:30:41 +0330

    final version of calculator project, i used function in this version.

--- calculator.py
+++ calculator.py
@@ -1,25 +1,37 @@
-# Version 2
+# Final Version
+def calculator(number1, number2, op):
+    if op == '+':
+        return number1 + number2
+    elif op == '-':
+        return number1 - number2
+    elif op == '*':
+        return number1 * number2
+    elif op == '/':
+        if number2 == 0:
+            return 0
+        else:
+            return number1 / number2
 
-print("Welcome to warrior calculator!\n")
-print("1. Sum(+)")
-print("2. Subtract(-)")
-print("3. Multiply(*)")
-print("4. Divide(/)")
-opt = int(input("\nPlease one of the above options: "))
-if opt <= 4:
-    num1 = int(input("Please Enter First Number: "))
-    num2 = int(input("Please Enter Second Number: "))
-    if opt == 1:
-        result = num1 + num2
-        print("{0} + {1} = {2}".format(num1, num2, result))
-    elif opt == 2:
-        result = num1 - num2
-        print("{0} - {1} = {2}".format(num1, num2, result))
-    elif opt == 3:
-        result = num1 * num2
-        print("{0} * {1} = {2}".format(num1, num2, result))
-    elif opt == 4:
-        result = num1 / num2
-        print("{0} / {1} = {2}".format(num1, num2, result))
-else:
-    print("Invalid Option!")
+
+print("Welcome To Warrior Calculator!!!")
+while True:
+    print('1. Sum(+)')
+    print('2. Subtract(-)')
+    print('3. Multiply(*)')
+    print('4. Divide(/)')
+    print('5. Exit')
+
+    opt = input("Please Enter You're Operator: ")
+    if opt == '5':
+        break
+
+    operators = '+-*/'
+    if opt == '1' or opt == '2' or opt == '3' or opt == '4':
+        opt = operators[int(opt) - 1]
+
+    num1 = int(input("Please Enter You're First Number: "))
+    num2 = int(input("Please Enter You're Second Number: "))
+
+    result = calculator(num1, num2, opt)
+
+    print("{0} {1} {2} = {3}".format(num1, opt, num2, result))
